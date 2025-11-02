efb8d5900e07da7e54b84767d9b4b949a0239604
cu83 <matthewloquinerioxx@gmail.com>
2025-11-02 16:33:03 +0800

    Test API-2

--- server/main.py
+++ server/main.py
@@ -8,35 +8,36 @@ from sqlalchemy.ext.declarative import declarative_base
 
 app = FastAPI()
 
-DATABASE_URL = 'postgresql://postgres:admin1234@localhost:5432/authentication_system'
+DATABASE_URL = "postgresql://postgres:admin1234@localhost:5432/authentication_system"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=False)
 
 db = SessionLocal()
 
-class UserCreate(BaseModel):
+class User(BaseModel):
     name: str
     email: str
     password: str
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'users'
+class UserCreate(Base):
+    __tablename__ = "users"
 
     id = Column(TEXT, primary_key=True)
     name = Column(VARCHAR(100))
     email = Column(VARCHAR(100))
     password = Column(LargeBinary)
 
-@app.post('/signup', status_code=200)
+@app.post('/signup', status_code=201)
 def signup_user(user: User):
-    user_db = db.query(User).filter(User.email == user.email).first()
+
+    user_db = db.query(UserCreate).filter(User.email == user.email).first()
 
     if user_db:
-        raise HTTPException(400, 'Email already exists.')
-    
+        HTTPException(400, 'Email already exists.')
+
     hashed_pw = bcrypt.hashpw(user.password.encode(), bcrypt.gensalt())
     user_db = User(id=str(uuid.uuid4()), name=user.name, email=user.email, password=hashed_pw)
 
