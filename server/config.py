8b2a5d0474224bde871ff15f5e535b589f1326ac
cu83 <matthewloquinerioxx@gmail.com>
2025-11-02 20:33:31 +0800

    Secured Database Credentials

---
+++ server/config.py
@@ -0,0 +1,19 @@
+from configparser import ConfigParser
+
+
+def config(filename='database.ini', section='postgresql'):
+    parser = ConfigParser()
+
+    parser.read(filename)
+    db = {}
+
+    if section in parser:
+        for key in parser[section]:
+            db[key] = parser[section][key]
+    
+    else:
+        raise Exception(
+            'Section {0} not found in {1} file'.format(section, filename)
+        )
+    return db
+
